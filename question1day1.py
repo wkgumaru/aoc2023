0b991ab04c56974de117164049cb45b264127b0d
wkgumaru <gumarukris@gmail.com>
2023-12-01 15:22:40 -0700

    finished the first puzzle and refactoring into one singular function

--- question1day1.py
+++ question1day1.py
@@ -3,20 +3,21 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-f = open("testfile.txt", "r")
-Lines = f.readlines()
-sum = 0
-for line in Lines:
-    number = ""
-    for char in line:
-        if char.isdigit():
-            number += char
-    if len(number) > 2:
-        number = number[0]+number[-1]
-    elif len(number) == 1:
-        number = number[0]+number[0]
-    print(number)
-    sum += int(number)
-print(sum)
+def question1():
+    f = open("testfile.txt", "r")  # opens text file
+    Lines = f.readlines()
+    sum = 0
+    for line in Lines:  # loop through file
+        number = ""
+        for char in line:
+            if char.isdigit():
+                number += char
+        lenNumber = len(number)
+        if lenNumber > 2:
+            number = number[0] + number[-1]
+        elif lenNumber == 1:
+            number = number[0] + number[0]
+        sum += int(number)
+    print(sum)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+question1()
